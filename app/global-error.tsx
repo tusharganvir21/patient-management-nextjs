849f2976c7acb3cadbf3fadd957fcbd5914775bd
unknown <tushar.ganvir21@vit.edu>
2024-09-10 19:30:42 +0530

    chnages

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import * as Sentry from "@sentry/nextjs";
 import NextError from "next/error";
 import { useEffect } from "react";
 
@@ -10,7 +8,6 @@ export default function GlobalError({
   error: Error & { digest?: string };
 }) {
   useEffect(() => {
-    Sentry.captureException(error);
   }, [error]);
 
   return (
